097c69a3c01727a7fa09c5a72c6b658ff6c10cc3
Khalil Camargo <kacamargopacker@gmail.com>
2024-05-13 14:09:10 -0300

    sectionFour

--- src/components/sectionFour.js
+++ src/components/sectionFour.js
@@ -6,13 +6,13 @@ const sectionFour = () => {
     <div className='sectionFour'>
             <div className='sectionFour-container'>
                 <div id='text' className='sectionFour-text'>
-                    <h1 id='title' className='sectionFour-text-title'>
-                        Nossa História!
-                    </h1>
                     <p className='sectionFour-text-paragraph'>Fundado em 1975. No início da década de 1980, 
                     o Dr. John Parker Hammond "teve" a ideia de clonar dinossauros a partir do 
                     DNA preservado em âmbarfossilizado. John Hammond criou um grupo de pesquisa, incluindo o Dr. Wu, para pôr este sonho em prática.
                     Eles conseguiram clonar um animal pré-histórico em 1984.</p>
+                    <p className='sectionFour-text-paragraph'>Em 1985, após essa conquista, Hammond conseguiu atrair investidores suficientes para fundar uma corporação inteira, a International Genetic Technologies, Inc. (InGen), dedicada à clonagem de vidas extintas. Sua missão era ser a principal empresa de pesquisa do mundo de ciência genética e biológica de ponta e tecnologia.
+
+A sede da InGen estava localizada em Palo Alto, CA, a pesquisa inicial também ocorreu lá. Mais tarde, a InGen transferiu maior parte de sua pesquisa para instalações na Ilha Sorna. Os dinossauros recém-nascidos cresciam e eram alimentados nesta ilha.</p>
                 </div>
                 <img className='sectionFour-img'src={ingenBuild} />
             </div>
--- src/components/sectionThree.js
+++ src/components/sectionThree.js
@@ -8,19 +8,28 @@ function SectionThree() {
   return (
     <div className="sectionThree">
         <div className="sectionThree-container">
-        <div className="sectionThree-text"><h2>Nossos projetos e parceiros!</h2></div>
+            <div className="sectionThree-text"><h2>Nossos projetos e parceiros!</h2></div>
             <div class="slider">
-            <div class="slide-track">
-            <div class="slide">
-                <img src={ingenLogo} height="100" width="100" alt="" />
-            </div>
-            <div class="slide">
-                <img src={jurasLogo} height="120" width="120" alt="" />
-            </div>
-            <div class="slide">
-                <img src={worldLogo} height="100" width="100" alt="" />
-            </div>
-            </div>
+                <div class="slide-track">
+                <div class="slide">
+                    <img src={ingenLogo} height="100" width="100" alt="" />
+                </div>
+                <div class="slide">
+                    <img src={jurasLogo} height="120" width="120" alt="" />
+                </div>
+                <div class="slide">
+                    <img src={worldLogo} height="100" width="100" alt="" />
+                </div>
+                <div class="slide">
+                    <img src={ingenLogo} height="100" width="100" alt="" />
+                </div>
+                <div class="slide">
+                    <img src={jurasLogo} height="120" width="120" alt="" />
+                </div>
+                <div class="slide">
+                    <img src={worldLogo} height="100" width="100" alt="" />
+                </div>
+                </div>
             </div>
         </div>
     </div>
