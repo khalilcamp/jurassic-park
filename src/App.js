d0b740ebe93d23a28be3a3ec5701458e688ae50f
Khalil Camargo <kacamargopacker@gmail.com>
2024-05-13 15:56:14 -0300

    tentativa de implantar emailJS

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
-import React from 'react';
+
+import React, { useRef } from 'react';
 import Navbar from './components/navbar';
 import SectionTwo from './components/sectionTwo';
 import SectionThree from './components/sectionThree';
-import SectionFour from './components/sectionFour'
+import SectionFour from './components/sectionFour';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import emailjs from '@emailjs/browser';
+
 
 function App() {
+    const form = useRef();
+  
+    const sendEmail = (e) => {
+      e.preventDefault();
+      emailjs
+      .sendForm('service_lsnges4', 'template_uxbam9d', form.current, {
+        publicKey: 'pfeXxWtN-GBaGmOv9',
+      })
+      .then(
+        () => {
+          console.log('SUCCESS!');
+        },
+        (error) => {
+          console.log('FAILED...', error.text);
+        },
+      );
+  };
+
   return (
     <>
       <Router>
@@ -24,6 +45,15 @@ function App() {
         <Route exact path='/' element={<SectionFour />} />
         </Routes>
       </Router>
+      <form ref={form} onSubmit={sendEmail}>
+      <label>Name</label>
+      <input type="text" name="user_name" />
+      <label>Email</label>
+      <input type="email" name="user_email" />
+      <label>Message</label>
+      <textarea name="message" />
+      <input type="submit" value="Send" />
+    </form>
     </>
   );
 }
