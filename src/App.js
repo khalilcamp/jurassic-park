9b21007f77187431347f4f29b2a2f2801d7ee99a
Khalil Camargo <kacamargopacker@gmail.com>
2024-05-13 16:07:07 -0300

    emailJS

--- src/App.js
+++ src/App.js
@@ -52,8 +52,8 @@ function App() {
       <input type="email" name="user_email" />
       <label>Message</label>
       <textarea name="message" />
-      <input type="submit" value="Send" />
-    </form>
+      <button type="submit" value="Send">SEND EMAIL</button>
+      </form>
     </>
   );
 }
